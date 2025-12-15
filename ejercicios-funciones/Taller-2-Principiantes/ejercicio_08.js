19c803bbdb7623f8994fecc88621bd32a1ea585f
HeiberD <heiberdiazp@gmail.com>
2025-12-15 10:37:44 -0500

    Se realiza y se prueba ejercicio_08.js

--- ejercicios-funciones/Taller-2-Principiantes/ejercicio_08.js
+++ ejercicios-funciones/Taller-2-Principiantes/ejercicio_08.js
@@ -1,4 +1,17 @@
 // Agregar propiedad a objeto:
 // Función agregarPropiedad(objeto, clave, valor) que añada una nueva propiedad a un objeto.
-// Ejemplo:
-// AgregarPropiedad({ nombre: "Luis"}, "edad", 25) → { nombre: "Luis",edad: 25 }
+// Ejemplo: AgregarPropiedad({ nombre: "Luis"}, "edad", 25) → { nombre: "Luis",edad: 25 }
+
+    let obj = { nombre: "Luis"};    
+    let propiedad = "edad";
+    let edad = 25
+    
+
+
+function agregarPropiedad(obj, propiedad, edad){
+   obj[propiedad] = edad    
+   return obj
+}
+
+agregarPropiedad(obj, propiedad, edad)
+console.log(obj)
